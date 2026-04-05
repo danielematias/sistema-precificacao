0fc1cdd9b76f7f5ed72ac460c0a419baf2ac5578
Daniele Matias <danielematias20@gmail.com>
2026-04-05 19:27:14 -0300

    Create tabela-vendedor.php with simplified product table

---
+++ tabela-vendedor.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <title>Tabela Vendedor</title>
+</head>
+<body>
+    <h1>Tabela de Vendedor</h1>
+    <table border='1'>
+        <tr>
+            <th>Nome do Produto</th>
+            <th>Preço de Lista</th>
+            <th>Percentual Máximo de Desconto</th>
+            <th>Preço Mínimo</th>
+        </tr>
+        <tr>
+            <td>Produto A</td>
+            <td>R$ 100,00</td>
+            <td>20%</td>
+            <td>R$ 80,00</td>
+        </tr>
+        <tr>
+            <td>Produto B</td>
+            <td>R$ 150,00</td>
+            <td>15%</td>
+            <td>R$ 127,50</td>
+        </tr>
+        <tr>
+            <td>Produto C</td>
+            <td>R$ 200,00</td>
+            <td>10%</td>
+            <td>R$ 180,00</td>
+        </tr>
+    </table>
+</body>
+</html>
